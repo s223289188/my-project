752860d02508c9f86cf78282a40bb9352f08b5ff
s223289188 <139200271+s223289188@users.noreply.github.com>
2023-09-27 03:56:01 +1000

    this is my second commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home-view">
     <h2 style="color: white;">Welcome to My Portfolio</h2>
     <p style="color: white;">
-      This is the home page of my portfolio website. You can add your content and customize it as needed.
+      welcome to my portfolio homepage.
     </p>
     <router-link to="/about" style="color: white;">Learn more about me</router-link>
   </div>
